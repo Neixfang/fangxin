4c6fd2fd2af23f8664ec81a8142c08c47578a36b
fangxin <616904997@qq.com>
2014-11-01 11:48:44 +0800

    Initial Commit

---
+++ c语言练习/Avki_fang/Avki_fang/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Avki_fang
+//
+//  Created by 方欣 on 14-11-1.
+//  Copyright (c) 2014年 方欣. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
