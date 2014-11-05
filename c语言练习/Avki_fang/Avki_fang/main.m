c947208f14d33c52a7eeb735281b43e14137aa60
fangxin <616904997@qq.com>
2014-11-05 11:09:28 +0800

    tasks

--- c语言练习/Avki_fang/Avki_fang/main.m
+++ c语言练习/Avki_fang/Avki_fang/main.m
@@ -1,17 +1,11 @@
-//
-//  main.m
-//  Avki_fang
-//
-//  Created by 方欣 on 14-11-1.
-//  Copyright (c) 2014年 方欣. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
-    return 0;
+#include<stdio.h>
+main()
+{
+    int i,j;
+    for(i=1;i<=9;i++)
+    {
+        for(j=1;j<=9;j++)
+            printf("%4d",i*j);
+        printf("\n");
+    }	
 }
